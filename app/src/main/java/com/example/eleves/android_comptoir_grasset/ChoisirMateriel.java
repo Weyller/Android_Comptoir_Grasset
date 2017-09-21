95b76af34b8a2b36d631e5630aa0187d19be74d7
weyller <weyller@gmail.com>
2017-09-21 15:29:24 -0400

    4 ieme commit layout materiel

---
+++ app/src/main/java/com/example/eleves/android_comptoir_grasset/ChoisirMateriel.java
@@ -0,0 +1,16 @@
+package com.example.eleves.android_comptoir_grasset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoisirMateriel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choisir_materiel);
+
+
+        setTitle("IG PRET");
+    }
+}
