189b0a0ecef04b441d9d299f241345481e6be0f3
weyller <weyller@gmail.com>
2017-09-21 17:12:24 -0400

    5 ieme commit layout display demande

---
+++ app/src/main/java/com/example/eleves/android_comptoir_grasset/Affichage_Demande.java
@@ -0,0 +1,13 @@
+package com.example.eleves.android_comptoir_grasset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Affichage_Demande extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_affichage__demande);
+    }
+}
