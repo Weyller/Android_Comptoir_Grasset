25f8b36ed943e5061bbd2030574cb70058f2693f
weyller <weyller@gmail.com>
2017-09-21 15:29:24 -0400

    4 ieme commit layout materiel

--- app/src/main/java/com/example/eleves/android_comptoir_grasset/NouveauPret.java
+++ app/src/main/java/com/example/eleves/android_comptoir_grasset/NouveauPret.java
@@ -1,9 +1,15 @@
 package com.example.eleves.android_comptoir_grasset;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NouveauPret extends AppCompatActivity {
+
+
+    Button btn_choix_materiel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,19 @@ public class NouveauPret extends AppCompatActivity {
 
 
         setTitle("IG Pret");
+
+        btn_choix_materiel = (Button)findViewById(R.id.btn_choix_materiel);
+
+        btn_choix_materiel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(NouveauPret.this, ChoisirMateriel.class);
+
+                startActivity(intent);
+            }
+        });
+
 
 
     }
